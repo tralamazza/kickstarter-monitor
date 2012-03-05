a9218cf0e7f7d484331c4e7cdb501bb5496b5396
Daniel Tralamazza <tralamazza@gmail.com>
2012-03-05 23:30:33 +0100

    .execute scans all recommnended projects

--- lib/kickstarter-monitor.rb
+++ lib/kickstarter-monitor.rb
@@ -1,7 +1,22 @@
+require "rubygems"
 require "kickstarter-monitor/version"
+require 'mongo'
+require 'kickstarter'
 
 module Kickstarter
   module Monitor
-    # Your code goes here...
+    class Service
+      def initialize
+        @db = Mongo::Connection.new.db("kickstarter")
+      end
+
+      def execute
+        coll = @db.collection("projects")
+        Kickstarter.by_list(:recommended) do |project|
+          coll.insert(project.to_hash)
+          puts project.name
+        end
+      end
+    end
   end
 end
